fe30a821a6ac6be760fb313863fba459b2c62abd
toddmnortonCO <toddmnorton@live.com>
2021-01-22 16:29:17 -0700

    mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const {ApolloServer} = require('apollo-server');
 const gql = require('graphql-tag');
+const mongoose = require('mongoose');
 
+const { MONGODB } = require('./config.js')
 
 const typeDefs = gql`
     type Query{
@@ -10,7 +12,7 @@ const typeDefs = gql`
 
 const resolvers = {
     Query: {
-        sayHi: () => 'Hello world!'
+        sayHi: () => 'Hello world!!!!!'
     }
 }
 
@@ -19,7 +21,11 @@ const server = new ApolloServer({
     resolvers
 });
 
-server.listen({ port: 5000})
-    .then(res => {
-        console.log(`Server running on ${res.url}`)
+mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true  })
+    .then(() => {
+        console.log('MongoDB connected')
+        return server.listen({ port: 5000 });
     })
+    .then((res) => {
+        console.log(`Server running at ${res.url}`)
+    });
