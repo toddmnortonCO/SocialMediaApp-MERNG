dc2b7388acd01b7fbda203638943fe057b1ed11e
toddmnortonCO <toddmnorton@live.com>
2021-01-23 11:44:40 -0700

    commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const {ApolloServer} = require('apollo-server');
 const mongoose = require('mongoose');
 
-const { MONGODB } = require('./config.js')
-const typeDefs = require('./graphql/typeDefs')
-const resolvers = require('./graphql/resolvers')
+const { MONGODB } = require('./config.js');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 
 const server = new ApolloServer({
     typeDefs,
