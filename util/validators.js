f6ad535ebca3ff722d7a9c0f8a7a25f418eb6bc6
toddmnortonCO <toddmnorton@live.com>
2021-01-23 12:37:51 -0700

    final save before lunch

--- util/validators.js
+++ util/validators.js
@@ -1,9 +1,9 @@
-module.exports.validateRegisterInput = {
+module.exports.validateRegisterInput = (
     username,
     email,
     password,
     confirmPassword
-} => {
+ ) => {
     const errors = {};
     if(username.trim() === ''){
         errors.username = 'Username must not be empty, please'
@@ -12,5 +12,27 @@ module.exports.validateRegisterInput = {
         errors.username = 'Email must not be empty, please'
     } else {
         const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/
+        if(!email.match(regEx)){
+            errors.email = 'Email must be a valid email address';
+        }
+    }
+    if (password === ''){
+        errors.passowrd = 'Password must not be empty';
+    } else if(password !== confirmPassword) {
+        errors.confirmPassword = 'Passwords must match';
+    }
+    return {
+        errors,
+        valid: Object.keys(errors).length < 1
+    }
+}
+
+module.exports.validateLoginInput = (username, password) => {
+    const errors = {};
+    if(username.trim() === ''){
+        errors.username = 'Username must not be empty'
+    }
+    if(username.trim() === ''){
+        errors.password = 'Password must not be empty'
     }
 }
