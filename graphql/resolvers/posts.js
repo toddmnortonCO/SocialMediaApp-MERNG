f8a12617d8660604147fbf5c015fb27984001586
toddmnortonCO <toddmnorton@live.com>
2021-01-24 11:15:44 -0700

    saving after post methods

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,14 +1,61 @@
-const Post = require('../../models/Post')
+const { AuthenticationError, UserInputError } = require("apollo-server");
+
+const Post = require("../../models/Post");
+const checkAuth = require("../../util/check-auth");
 
 module.exports = {
-    Query: {
-        async getPosts() {
-            try{
-                const posts = await Post.find();
-                return posts;
-            } catch(err){
-                throw new Error(err);
-            }
+  Query: {
+    async getPosts() {
+      try {
+        const posts = await Post.find().sort({ createdAt: -1 });
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+    async getPost(_, { postID }) {
+      try {
+        const post = await Post.findByID(postID);
+        if (post) {
+          return post;
+        } else {
+          throw new Error("Post not found");
         }
-    }
-}
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+  Mutation: {
+    async createPost(_, { body }, context) {
+      const user = checkAuth(context);
+      console.log(user);
+
+      const newPost = new Post({
+        body,
+        user: user.id,
+        username: user.username,
+        createdAt: new Date().toISOString(),
+      });
+
+      const post = await newPost.save();
+
+      return post;
+    },
+    async deletePost(_, { postID }, context) {
+      const user = checkAuth(context);
+
+      try {
+        const post = await post.findByID(postId);
+        if (user.username === post.username) {
+          await post.delete();
+          return "Post deleted successfully";
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } catch(err){
+          throw new Error(err);
+      }
+    },
+  },
+};
--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const {ApolloServer} = require('apollo-server');
-const mongoose = require('mongoose');
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
 
-const { MONGODB } = require('./config.js');
-const typeDefs = require('./graphql/typeDefs');
-const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require("./config.js");
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req }),
 });
 
-mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true  })
-    .then(() => {
-        console.log('MongoDB connected')
-        return server.listen({ port: 5000 });
-    })
-    .then((res) => {
-        console.log(`Server running at ${res.url}`)
-    });
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB connected");
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
